fccbb19000bc183a31c75f72691ddbeb4478cc4e
Sagar Rajput <rajputsagar716@gmail.com>
2024-03-31 11:51:13 +0545

    added edit and add product functionality

--- onlinemarketplace/userprofile/views.py
+++ onlinemarketplace/userprofile/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
+
+from store.models import Product
 
 from .models import UserProfile
+from store.forms import ProductForm
 
 # Create your views here.
 def vendor_detail(request, pk):
@@ -13,6 +18,48 @@ def vendor_detail(request, pk):
         'user': user
     })
 
+@login_required
+def my_store(request):
+    return render(request,'userprofile/my_store.html')
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = request.POST.get('title')
+            product = form.save(commit=False)
+            product.user = request.user
+            product.slug = slugify(title)
+            product.save()
+            return redirect('my_store')
+    else:
+            form = ProductForm()
+
+    return render(request, 'userprofile/add_product.html',{
+        'title': 'Add Product',
+        'form': form,
+    })
+
+@login_required
+def edit_product(request, pk):
+    product = Product.objects.filter(user=request.user).get(pk=pk)
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('my_store')
+    
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'userprofile/add_product.html',{
+        'title': 'Edit Product',
+        'form': form,
+    })
+
+@login_required
 def account_detail(request):
     return render(request, 'userprofile/account_detail.html')
 
